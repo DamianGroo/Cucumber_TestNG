b751f62e71c6afa48a28bdad807c6965adcb99b8
DamianGRO <fiszher@gmail.com>
2020-07-17 20:00:47 +0200

    modyfikacja Runnera - Ready

--- src/main/java/testRunnerTestNG/RunnerTestNG.java
+++ src/main/java/testRunnerTestNG/RunnerTestNG.java
@@ -10,30 +10,14 @@ import org.testng.annotations.Test;
 import org.testng.annotations.DataProvider;
 
 @CucumberOptions(
-       //  features = "./features"
         features = "E:\\CUCUMBER BIBLIOTEKI I WSZYSTKO\\ProjektMavenTestNGwithCucumber\\src\\main\\java\\featuresTestNG"
-      // zle  features = {"E:\\CUCUMBER BIBLIOTEKI I WSZYSTKO\\ProjektMavenTestNGwithCucumber\\src\\test\\java\\featuresTestNG"}
-      //  , glue = {"E:\\CUCUMBER BIBLIOTEKI I WSZYSTKO\\ProjektMavenTestNGwithCucumber\\src\\main\\java\\stepsDefinitionsTestNG"}
        , glue = {"stepsDefinitionsTestNG"}
-      // zle  , glue = {"E:\\CUCUMBER BIBLIOTEKI I WSZYSTKO\\ProjektMavenTestNGwithCucumber\\src\\test\\java\\stepsDefinitionsTestNG"}
        , tags = {"@grupaScenariuszy"}
-      //  , tags = {"@test5_DodaniePrzedmiotuDoKoszyka"}
-     //   , tags = {"@test1_poprawneLogowanie"}
-     //   , plugin = {"pretty", "html:target/site/cucumber-pretty", "json:target/cucumber.json"}
-      //  , tags = {"@test5_DodaniePrzedmiotuDoKoszyka"}
-
-     //   , tags = {"@test_z_waiterem"}
-
-
-     //   , monochrome = true
 )
 
 
 
 public class RunnerTestNG {
-
-
-
     private TestNGCucumberRunner testNGCucumberRunner;
 
     @BeforeClass(alwaysRun = true)
@@ -41,18 +25,11 @@ public class RunnerTestNG {
         testNGCucumberRunner = new TestNGCucumberRunner(this.getClass());
     }
 
-
-
-
-
-
     @Test(dataProvider =  "features")
     public void feature(PickleEventWrapper eventwrapper, CucumberFeatureWrapper cucumberFeature) throws Throwable {
         //testNGCucumberRunner.runCucumber(cucumberFeature.getCucumberFeature());
         testNGCucumberRunner.runScenario(eventwrapper.getPickleEvent());
     }
-
-
 
     @DataProvider//(parallel=true)
     public Object[][] features() {
@@ -64,7 +41,4 @@ public class RunnerTestNG {
     public void tearDownClass() throws Exception {
         testNGCucumberRunner.finish();
     }
-
-
-
 }
